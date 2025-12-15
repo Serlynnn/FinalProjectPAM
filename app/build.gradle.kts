3d606ab21c4027dec0083b7eeaf5c1f2b9174dcb
chashf <142861294+chashf@users.noreply.github.com>
2025-12-16 00:09:52 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -73,6 +74,5 @@ dependencies {
 
 	implementation("androidx.compose.material:material-icons-extended")
 	implementation("androidx.compose.material3:material3")
-	implementation("io.coil-kt:coil-compose:2.6.0")
 
 }
